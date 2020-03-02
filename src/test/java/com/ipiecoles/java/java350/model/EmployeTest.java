b6df7860840cbef6a209b5cb06cd5a151bfb25a8
malikajer <malikajerrar@gmail.com>
2020-03-02 14:49:22 +0100

    test

---
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -0,0 +1,19 @@
+package com.ipiecoles.java.java350.model;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+
+public class EmployeTest {
+    @Test
+    public void testGetNbAnneeAncienneteDtEmbaucheNull(){
+        //Given
+        Employe employe = new Employe();
+        employe.setDateEmbauche(null);
+        //When
+        Integer nbAnneeAnciennete = employe.getNombreAnneeAnciennete();
+        //Then
+        Assertions.assertThat(nbAnneeAnciennete).isEqualTo(0);
+    }
+}
